a80fd901101c25030226e803331599bf37e02377
Matt Crouch <github@mattcrouch.net>
2015-05-17 15:50:13 +0100

    Replace blanked credentials file with example file

---
+++ credentials-example.php
@@ -0,0 +1,7 @@
+<?php
+define('JUST_GIVING_API_KEY', "x");
+define('SERVER_NAME', "x");
+define('SERVER_USERNAME', "x");
+define('SERVER_PASS', "x");
+define('DB_NAME', "x");
+define('GOOGLE_MAPS_API_KEY', "x");
--- credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('JUST_GIVING_API_KEY', "");
-define('SERVER_NAME', "");
-define('SERVER_USERNAME', "");
-define('SERVER_PASS', "");
-define('DB_NAME', "");
